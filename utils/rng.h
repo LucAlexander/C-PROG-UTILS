930fdec50188d21d331bee6681e8162b3e638f97
LucAlexander <46453661+LucAlexander@users.noreply.github.com>
2021-08-27 14:45:32 -0500

    random number generator
    wrapper with some simple utility functions for random number generation in c

---
+++ utils/rng.h
@@ -0,0 +1,18 @@
+#ifndef RANDNUMGEN_H
+#define RANDNUMGEN_H
+
+#include <inttypes.h>
+
+void startRandSeed();
+
+int32_t randRange(int32_t a, int32_t b);
+
+uint32_t rollDie(uint32_t sides);
+
+int32_t randChoiceI(uint32_t n, ...);
+
+double randChoiceF(uint32_t n, ...);
+
+const char* randChoiceS(uint32_t n, ...);
+
+#endif
